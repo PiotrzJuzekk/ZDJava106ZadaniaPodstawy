84df1c7ebd1d2a8ca1186d1b9a3e1ff09bec54e5
Pjoter K <piotrz9522@gmail.com>
2022-03-06 10:59:19 +0100

    Task

---
+++ src/main/java/pl/sdacademy/java/basic/exercises/day3/Task4.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.java.basic.exercises.day3;
+
+public class Task4 {
+
+    private static final String SNEEZE_REGEX = "";
+
+    public static void main(String[] args) {
+
+    }
+}
